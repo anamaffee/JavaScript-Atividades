c1a0c925bec32e884bc228e909a032bd5b7e2c76
aninha <anamaffe8@hotmail.com>
2026-03-13 11:45:09 -0300

    Problemas com o código
    Resolvendo problemas de aparencia

---
+++ script.js
@@ -0,0 +1,9 @@
+ const caixote = document.getElementById("caixote");
+      caixote.classList.add("");
+
+
+const btnTrocaImg = document.getElementsByClassName("controle")
+    btnTrocaImg.addEventListener("click",()=>{
+          caixaTexto.classList.remove("apagada");
+          caixaTexto.classList.add("acesa");
+    });
